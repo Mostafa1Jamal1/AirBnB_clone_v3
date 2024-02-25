0169666962578f85dcf59dfc4f458a4df57196d0
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-02-25 03:59:51 +0200

    feat: Create a new view for State objects that handles all default RESTFul API actions.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """ API module """
 
+from api.v1.views import app_views  # noqa: E402
 from flask import Flask
 from models import storage
 from os import getenv
+
+
 app = Flask(__name__)
-from api.v1.views import app_views  # noqa: E402
+app.url_map.strict_slashes = False
+
+host = getenv('HBNB_API_HOST', '0.0.0.0')
+port = getenv('HBNB_API_PORT', '5000')
+
 app.register_blueprint(app_views)
 
 
@@ -21,8 +28,4 @@ def teardown_appcontext(response_or_exc):
 
 
 if __name__ == "__main__":
-    app.run(host=(getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST')
-                  else '0.0.0.0'),
-            port=(getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT')
-                  else 5000),
-            threaded=True)
+    app.run(host=host, port=port, threaded=True),
